a7a5c2178520128fd51d875b980b16bfcc66c139
Tobias Bossert <tobi@Tobiass-MacBook-Air.local>
2025-12-05 17:53:37 +0100

    first solution finally

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -14,29 +14,30 @@ impl Dial {
     }
 
     fn rotate_right(&mut self, n: usize) {
-        let new_val = self.value + n;
-        if new_val > 99 {
-            self.value = new_val - 100;
-        } else {
-            self.value = new_val;
+        let mut n = n;
+        while n > 0 {
+            if self.value == 99 {
+                self.value = 0;
+            } else {
+                self.value += 1;
+            }
+            n -= 1;
         }
+
         if self.value == 0 {
             self.result += 1;
         }
     }
 
-    // TODO: we need to properly evaluate every rotation
-    // else we lose some 0s
     fn rotate_left(&mut self, n: usize) {
         let mut n = n;
-        if n > 99 {
-            n %= 100;
-        }
-
-        if let Some(new) = self.value.checked_sub(n) {
-            self.value = new;
-        } else {
-            self.value = 100 - self.value.abs_diff(n);
+        while n > 0 {
+            if self.value == 0 {
+                self.value = 99;
+            } else {
+                self.value -= 1;
+            }
+            n -= 1;
         }
 
         if self.value == 0 {
@@ -77,11 +78,9 @@ fn test1() {
     L99
     L122";
     let mut dial = Dial::new();
-    //println!("The dial starts by pointing at 50.");
     for line in test_input.lines() {
         let line = line.trim();
         parse_and_apply_line(line, &mut dial);
-        //println!("The dial is rotated {line} to point at {}.", dial.value);
     }
 
     assert_eq!(3, dial.result);
@@ -89,29 +88,14 @@ fn test1() {
 
 #[test]
 fn test2() {
+    println!("T2 Start ------");
     let test_input = "L50
         L50
         L1";
     let mut dial = Dial::new();
-    //println!("The dial starts by pointing at 50.");
     for line in test_input.lines() {
         let line = line.trim();
         parse_and_apply_line(line, &mut dial);
-        //println!("The dial is rotated {line} to point at {}.", dial.value);
-    }
-
-    assert_eq!(1, dial.result);
-}
-
-#[test]
-fn test3() {
-    let test_input = "L101";
-    let mut dial = Dial::new();
-    println!("The dial starts by pointing at 50.");
-    for line in test_input.lines() {
-        let line = line.trim();
-        parse_and_apply_line(line, &mut dial);
-        println!("The dial is rotated {line} to point at {}.", dial.value);
     }
 
     assert_eq!(1, dial.result);
